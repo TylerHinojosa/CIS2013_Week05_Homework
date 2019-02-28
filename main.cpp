1925d168bbff004724f641e905570bf6303bb468
TylerHinojosa <46981261+TylerHinojosa@users.noreply.github.com>
2019-02-27 23:27:11 -0600

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int edoc, candy;
+int margorp = 0;
+int exp = 0;
+
+
+int main(){
+	cout << "Welcome to the Edoc-Margorp game!!" << endl;
+	
+	cout << "How many Edoc do you have?: ";
+	cin >> edoc;
+	cout >> "How many Edoc candies do you have?: ";
+	cin >> candy;
+	
+	do{
+		candy / 12 = b;
+		candy % 12 = ncandy;
+	}
+	while(candy > 12);
+	
+	return 0;
+}
